0506d1adbfc4a1d460ac57c2957c77c115a86c28
Ravi Ranjan <raviranjan091@gmail.com>
2018-02-17 14:59:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,12 @@
 console.log('Loaded!');
 
+//change the text
 var element=document.getElementById("main-text");
 
 element.innerHTML="New Value";
+
+//move the image
+var img=document.getElementById("madi");
+img.onclick = function(){
+  img.style.marginLeft="100px";  
+};
