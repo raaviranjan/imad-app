1c9f726b4b7456d527410bda370aaa1a8595f665
Ravi Ranjan <raviranjan091@gmail.com>
2018-02-17 17:24:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,8 +40,7 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput=document.getElementById("name");
-var name=nameInput.value;
+
 var submit=document.getElementById("submit_btn");
 submit.onclick= function(){
     
@@ -68,6 +67,8 @@ submit.onclick= function(){
   };
   
   //make a request
+  var nameInput=document.getElementById("name");
+  var name=nameInput.value;
   request.open('GET','http://raviranjan091.imad.hasura-app.io/submit-name?name=' + name,true);
   request.send(null);
     
