24082a43e766b4d34b41e6fafba07ccf8107bc17
Ravi Ranjan <raviranjan091@gmail.com>
2018-02-17 16:45:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,3 +38,19 @@ button.onclick = function(){
   request.open('GET','http://raviranjan091.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+
+//submit name
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onCLick= function(){
+  //make a request to server and send the names
+  //capture the name and render it as a list
+  var names=['name1','name2','name3','name4'];
+  var list="";
+  for (var i=0;i<names.length();i++){
+      list="<li>" + names[i] + "</li>";
+  }
+  val ul=document.getElementById("namelist");
+  ul.innerHTML=list;
+};
