ace5975265e13cf91e575e7ee803eefb250d8b1d
Ravi Ranjan <raviranjan091@gmail.com>
2018-02-27 23:49:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,11 +128,11 @@ app.get('/article/:articleName',function(req, res){
     //articleName==article-one
     var articleName=req.params.articleName;
     
-    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err,result){
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } else{
-           if(result.rows.length == 0){
+           if(result.rows.length === 0){
                res.status(400).send("Article Not Found");
            }else{
                var articleData = result.rows[0];
