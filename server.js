8bc5e2d6c7aab6b1088c10be0a9769b276c335a2
Ravi Ranjan <raviranjan091@gmail.com>
2018-02-27 23:17:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user:'raviranjan091',
+    database:'raviranjan091',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -77,6 +86,19 @@ function createTemplate(data){
     `;
     return htmlTemplate;
 }
+
+var pool = new Pool(config);
+app.get('/test-db'.function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 
 var counter=0;
 app.get('/counter',function(req,res){
