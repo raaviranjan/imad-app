35bd05f7014b882609877a784437da9b5a23dd05
Ravi Ranjan <raviranjan091@gmail.com>
2018-03-01 23:02:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
-var bodyParser = require('bodyParser');
+//var bodyParser = require('bodyParser');
 
 var config = {
     user:'raviranjan091',
@@ -15,7 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.JSON());
+//app.use(bodyParser.JSON());
 
 function createTemplate(data){
     var title = data.title;
@@ -66,7 +66,20 @@ app.get('/hash/:input',function(req,res){
    res.send(hashedString);
 });
 
-
+/*app.post('/create-user',function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)', [username,dbString], function(err,result){
+       if(err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send('User successfully created: '+ username);
+        }
+   });
+});*/
 
 
 var pool = new Pool(config);
