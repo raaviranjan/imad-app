cbde1e4c3068a259c436d6b6647428635f290bcd
Ravi Ranjan <raviranjan091@gmail.com>
2018-03-03 22:38:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,52 +1,6 @@
 console.log('Loaded!');
 
-
-//move the image
-var img=document.getElementById("madi");
-
-var marginLeft=0;
-
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-};
-
-//blog content
-var heading = document.getElementById("blogHeading");
-heading.innerHTML="gfchjjg";
-
-
-
-
-//button counter
-var button=document.getElementById("counter");
-button.onclick = function(){
-  //create a request oblect
-  var request = new XMLHttpRequest();
-  
-  //capture the responce and store it in a variable
-  request.onreadystatechange = function(){
-      if(request.readyState == XMLHttpRequest.DONE)
-      {
-          //take some action
-          if(request.status == 200){
-              var counter=request.responseText;
-              var span=document.getElementById("count");
-              span.innerHTML=counter.toString();
-          }
-      }
-  };
-  
-  //make a request
-  request.open('GET','http://raviranjan091.imad.hasura-app.io/counter',true);
-  request.send(null);
-};
-
-//submit name
+//submit username/password
 
 var submit=document.getElementById("submit_btn");
 submit.onclick= function(){
@@ -59,25 +13,26 @@ submit.onclick= function(){
       if(request.readyState == XMLHttpRequest.DONE)
       {
           //take some action
-          if(request.status == 200){
+          if(request.status === 200){
               //capture the name and render it as a list
-              var names=request.responseText;
-              names=JSON.parse(names);
-              var list="";
-              for (var i=0;i<names.length;i++){
-                  list += "<li>" + names[i] + "</li>";
-              }
-              var ul=document.getElementById("namelist");
-              ul.innerHTML=list;
+              console.log('user logged in successfully');
+              alert('Logged in successfully');
+          }else if(request.status === 403){
+              alert('username/password is incorrect');
+          }else if(request.status === 500){
+              alert('something went wrong on the server');
           }
       }
   };
   
   //make a request
-  var nameInput=document.getElementById("name");
-  var name=nameInput.value;
-  request.open('GET','http://raviranjan091.imad.hasura-app.io/submit-name?name=' + name,true);
-  request.send(null);
+  var username=document.getElementById("username").value;
+  var password=document.getElementById("password").value;
+  console.log(username);
+  console.log(password);
+  request.open('POST','http://raviranjan091.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username: username, password: password}));
     
   //make a request to server and send the names
   
