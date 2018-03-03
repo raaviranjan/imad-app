c6d97b7f654842dc824bde8847f87d148ce3dc9e
Ravi Ranjan <raviranjan091@gmail.com>
2018-03-03 22:56:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,9 +119,9 @@ app.post('/login',function(req,res){
 
 app.get('/check-login',function(req,res){
    if(req.session && req.session.auth && req.session.auth.userId){
-       req.send('You are logged in with ' + req.session.auth.userId.toString());
+       res.send('You are logged in with ' + req.session.auth.userId.toString());
    }else{
-       req.send('You are not logged in');
+       res.send('You are not logged in');
    }
 });
 
